0b99bf2f4cf873ff53499a89ceb3c4519e2adf01
David Atat <davidatat8@gmail.com>
2025-05-02 16:39:24 +0100

    Added Auto scroll

--- src/components/Home/Testimonial/Testimonial.jsx
+++ src/components/Home/Testimonial/Testimonial.jsx
@@ -1,5 +1,9 @@
 /** @format */
 
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import styles from "./Testimonial.module.css";
 
 const Testimonial = () => {
@@ -7,32 +11,55 @@ const Testimonial = () => {
     {
       quote:
         '"I really do appreciate your good work and I am confident to stand as a referral to more clients in the future."',
+      attribution: "Mr. Jude Joseph MD and CEO \n Soil Foods Services Ltd.",
     },
     {
-      quote: '"I like your work."',
+      quote: '\n"I like your work."',
+      attribution: "MD, Royal Albert Properties Limited",
     },
     {
       quote:
         '"This is great. Many thanks to you and Ehis for your work on this case."',
+      attribution: "Dr. Kene Onwugaje MD, REDUS CENTER FOR DIGESTIVE HEALTH LTD",
     },
   ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className={styles.testimonialSection}>
       <h2 className={styles.heading}>Testimonials</h2>
-      <div className={styles.testimonialContainer}>
+      <Slider {...settings}>
         {testimonials.map((testimonial, index) => (
           <div key={index} className={styles.testimonialCard}>
-            {/* <div> */}
             <div className={styles.cardContent}>
-              <p id='quote-text' className={styles.quoteText}>
+              <p id="quote-text" className={styles.quoteText}>
                 {testimonial.quote}
               </p>
+              <div className={styles.attribution}>
+                <span>{testimonial.attribution}</span>
+              </div>
             </div>
-            {/* </div> */}
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
